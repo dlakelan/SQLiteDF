894f6e9b43cf3c0f36e2d97c053fa19f16173bfe
Daniel Lakeland <dlakelan@street-artists.org>
2021-07-14 11:20:09 -0700

    add compat entry for julia, add simple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,3 +10,20 @@ c = @sqlitedf(a,b,"select a.*,b.baz from a join b on a.foo=b.foo") |> DataFrame
 @test c == cc
 
 
+a = DataFrame(foo=["one","two","three"],bar=[1,2,3])
+b = DataFrame(bar=[1,2,3],baz=["ONE","TWO","THREE"])
+cc = DataFrame(foo=["one","two","three"],bar=[1,2,3],baz=["ONE","TWO","THREE"])
+
+c = @sqlitedf(a,b,"select a.*,b.baz from a join b on a.bar=b.bar") |> DataFrame
+
+@test c == cc
+
+
+
+a = DataFrame(foo=["one","two","three"],bar=[1,2,3])
+b = DataFrame(foo=["one","two","three"],baz=["ONE","TWO","THREE"])
+cc = DataFrame(foo=["one","two","three"],bar=[1,2,3],baz=["ONE","TWO","THREE"])
+
+c = @sqlitedf(a,b,"select a.*,b.baz from a join b on a.foo=b.foo") |> DataFrame
+
+@test c == cc
